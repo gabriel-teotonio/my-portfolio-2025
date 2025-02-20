9845f81a8fe92cacacdaa8581014eec697d53ab9
Gabriel Teotonio <gabriel23teotonio@gmail.com>
2025-02-19 22:44:42 -0300

    content hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,16 @@ export default function Home() {
       >
         <div className="max-w-[1200px] mx-auto bg-blue-600">
         <HeaderMain />
+        <div className="">
+          <div className="">
+            <h1 className="text-lg">Tranformando suas ideias 
+            em realidade com tecnologia
+            e criatividade.</h1>
+            <p className="">Se você precisa de um site personalizado, automação eficiente ou um chatbot avançado com IA, eu crio a solução ideal para você.</p>
+            <button className="bg-orange-500 p-2 ">Solicite um orcamento</button>
+          </div>
+          <Image src="/assets/images/teki.png" alt="Logo" width={100} height={100} />
+        </div>
         </div>
       </main>
 
