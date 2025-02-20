49e240cca865d0f92c15fb7e29ae3089128ce04a
Gabriel Teotonio <gabriel23teotonio@gmail.com>
2025-02-19 22:39:23 -0300

    add hero section

---
+++ src/components/HeaderMain.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export const HeaderMain = () => {
+    return (
+        <header className="w-[1200px] flex items-center p-4
+         bg-opacity-10 bg-slate-300 rounded-lg fixed top-4
+         justify-between
+         "
+         >
+            <Image src="/assets/icons-svg/logo-teki.svg" alt="Logo" width={100} height={100} />
+
+            <ul className="flex gap-4 text-sm">
+                <a 
+                className="font-semibold hover:text-orange-500 transition-all duration-200" 
+                href="#"
+                >Home</a>
+                <a 
+                className="font-semibold hover:text-orange-500 transition-all duration-200" 
+                href="#"
+                >Sobre</a>
+                <a 
+                className="font-semibold hover:text-orange-500 transition-all duration-200" 
+                href="#"
+                >Projetos</a>
+                <a 
+                className="font-semibold hover:text-orange-500 transition-all duration-200" 
+                href="#"
+                >Contato</a>
+            </ul>
+        </header>
+    )
+};
